0244e0e2ab40707d9310c078c313e219a603d7f2
Bjørn Fjukstad <bfj@dips.no>
2020-07-02 13:33:52 +0200

    ask for permission before removing any directories

--- Program.cs
+++ Program.cs
@@ -5,15 +5,24 @@ namespace scrub
 {
     class Program
     {
+        /// <summary>
+        /// A simple tool for removing `bin` and `obj` folders. Will recursively
+        /// traverse the current working directory and remove folders as it finds
+        /// them.
+        /// </summary>
         /// <param name="path">
         ///     Where to start scrubbing away `bin` and `obj` folders
         /// </param>
-        static void Main(string path = ".")
+        /// <param name="ask">
+        ///     Ask before removing a folder. Set to `false` to skip any user
+        ///     interaction
+        /// </param>
+        static void Main(string path = ".", bool ask = true)
         {
-            RemoveBinAndObj(path);
+            RemoveBinAndObj(path, ask);
         }
 
-        static void RemoveBinAndObj(string path)
+        static void RemoveBinAndObj(string path, bool askForPermission)
         {
             var folders = Directory.EnumerateDirectories(path);
             foreach (var folder in folders)
@@ -21,12 +30,39 @@ namespace scrub
                 var folderInfo = new DirectoryInfo(folder);
                 if (folderInfo.Name == "bin" || folderInfo.Name == "obj")
                 {
-                    Console.WriteLine($"Removing '{folderInfo.FullName}'");
-                    Directory.Delete(folderInfo.FullName, true);
+                    var shouldRemoveFolder = false;
+
+                    if (askForPermission)
+                    {
+                        Console.WriteLine($"Should I remove '{folderInfo.FullName}'? y/n ");
+                        var answer = Console.ReadLine();
+                        if (answer == "y")
+                        {
+                            shouldRemoveFolder = true;
+                        }
+                        else
+                        {
+                            shouldRemoveFolder = false;
+                        }
+                    }
+                    else
+                    {
+                        shouldRemoveFolder = true;
+                    }
+
+                    if (shouldRemoveFolder)
+                    {
+                        Console.WriteLine($"Removing '{folderInfo.FullName}'");
+                        Directory.Delete(folderInfo.FullName, true);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Skipping '{folderInfo.FullName}'");
+                    }
                 }
                 else
                 {
-                    RemoveBinAndObj(folder);
+                    RemoveBinAndObj(folder, askForPermission);
                 }
             }
         }
--- README.md
+++ README.md
@@ -1,21 +1,36 @@
 ![.NET Core](https://github.com/fjukstad/scrub/workflows/.NET%20Core/badge.svg)
 ![https://www.nuget.org/packages/scrub](https://img.shields.io/nuget/v/scrub?color=success&logoColor=white&logo=NuGet)
+
 # scrub
+
 A simple `dotnet` tool for cleaning out any `bin` or `obj` folders. Especially
 useful if you are switching between developing on Windows and WSL with Ubuntu.
 The only difference between `scrub` and
 [`clean`](https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-clean) is
 that `scrub` removes everything within the `bin` or `obj` folders, not just the
-build artifacts created by `MSBuild`. 
-
+build artifacts created by `MSBuild`.
 
 # Usage
 
 Simply run `scrub` in any folder you want to clean out. It will recursively
-traverse any folder and remove all `bin` and `obj` folders it encounters.  
+traverse any folder and remove all `bin` and `obj` folders it encounters.
+
+## Example
 
 ```
 bjorn:/mnt/c/src/scrub$ scrub
+Should I remove '/mnt/c/src/scrub/bin'? y/n
+y
+Removing '/mnt/c/src/scrub/bin'
+Should I remove '/mnt/c/src/scrub/obj'? y/n
+y
+Removing '/mnt/c/src/scrub/obj'
+```
+
+You can skip user interaction by running `scrub` with `--ask false`:
+
+```
+bjorn:/mnt/c/src/scrub$ scrub --ask false
 Removing '/mnt/c/src/scrub/bin'
 Removing '/mnt/c/src/scrub/obj'
 ```
@@ -23,19 +38,15 @@ Removing '/mnt/c/src/scrub/obj'
 You can also point it to where it should scrub await folders with `--path`
 
 ```
-bjorn:/mnt/c/src$ scrub --path scrub/
+bjorn:/mnt/c/src$ scrub --path scrub/ --ask false
 Removing '/mnt/c/src/scrub/bin'
 Removing '/mnt/c/src/scrub/obj'
 ```
 
 # Install
+
 Install `scrub` globally with
 
 ```
 dotnet tool install --global scrub
-``` 
-
-
-
-
-
+```
